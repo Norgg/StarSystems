0842f690ea71f9d8f140d0a507cca769f6efe326
medsouz <medsouz99@gmail.com>
2014-11-06 10:39:09 -0500

    Changed to .NET 3.5 for better Unity compatibility

--- src/StarSystem.cs
+++ src/StarSystem.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using StarSystems.Creator;
 using StarSystems.Data;
 using StarSystems.Utils;
